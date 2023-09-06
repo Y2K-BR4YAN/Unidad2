53d519b3a4c3a5146fe888411e6be4cc049fb77c
Y2K-BR4YAN <143621993+Y2K-BR4YAN@users.noreply.github.com>
2023-09-06 08:40:10 -0600

    Add files via upload

---
+++ ListadeTareas.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variablesbmc;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ListadeTareas {
+     public static void main(String[] args) {
+    String tarea = "Hacer tareas";
+    String bañar = "Bañar a mi perro";
+    String hermana= "Ir por mi hermana";
+    String lavar = "Lavar el carro";
+    String trastes = "Lavar los trastes";
+    String baño = "Lavar el baño";
+    
+   System.out.println("Tus tareas pendientes son " + tarea);
+   System.out.println("Baña al perro" + bañar);
+   System.out.println("ve por tu hermana" + hermana);
+   System.out.println("Lava tu carro" + lavar);
+   System.out.println("Lava los trastes" + trastes);
+   System.out.println("Lava el baño" + baño);
+   
+           
+   
+   
+   
+
+}}
